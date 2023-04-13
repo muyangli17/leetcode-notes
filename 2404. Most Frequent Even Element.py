df4f6248ccb3dd676bc29374c5f1636434ca8881
Muyang.Li <1357351508@qq.com>
2023-04-13 18:02:50 +0800

    230413每日一题

---
+++ 2404. Most Frequent Even Element.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        record = dict()
+        for num in nums:
+            if num % 2 == 0:
+                if num in record:
+                    record[num] += 1
+                else:
+                    record[num] = 1
+
+        maxCount = 0
+        maxNum = -1
+        for num in record:
+            if record[num] > maxCount:
+                maxNum = num
+                maxCount = record[num]
+            if record[num] == maxCount and num < maxNum:
+                num = maxNum
+        return maxNum
